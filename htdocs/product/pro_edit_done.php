4a996d151968904ab426435218719ccdda666803
ryu-sugi <81607989+ryu-sugi@users.noreply.github.com>
2021-10-19 14:52:55 +0900

    商品情報修正機能追加

---
+++ htdocs/product/pro_edit_done.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+ <meta charset="UTF-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>ろくまる農園</title>
+</head>
+<body>
+ 
+<?php 
+
+try
+{
+    $pro_code = $_POST['code'];
+    $pro_name = $_POST['name'];
+    $pro_price = $_POST['price'];
+
+    $pro_code = htmlspecialchars($pro_code,ENT_QUOTES,'UTF-8');
+    $pro_name = htmlspecialchars($pro_name,ENT_QUOTES,'UTF-8');
+    $pro_price = htmlspecialchars($pro_price,ENT_QUOTES,'UTF-8');
+    // ↓教科書と開発環境が違う為要注意：[host=localhost]ではなく、dockerで使われているIPAddressを確認して[host=IPAddress]を入力する。
+    $dsn = 'mysql:dbname=shop;host=172.19.0.4;port=3306;charset=utf8';
+    $user = 'root';
+    $password = 'password';
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = 'UPDATE mst_product SET name=?,price=? WHERE code=?';
+    $stmt = $dbh->prepare($sql);
+    $data[] = $pro_name;
+    $data[] = $pro_price;
+    $data[] = $pro_code;
+    $stmt->execute($data);
+
+    $dbh = null;
+
+    print '修正しました。<br/>';
+
+}
+    catch (Exception $e)
+{
+    print 'ただいま障害により大変ご迷惑をお掛けしております。';
+    exit();
+}
+
+?>
+
+    <a href="pro_list.php">戻る</a>
+
+</body>
+</html>
