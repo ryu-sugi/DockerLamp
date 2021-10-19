50d65dbe8c4c3381fb85f54ea6dd34e163487aab
ryu-sugi <81607989+ryu-sugi@users.noreply.github.com>
2021-10-19 21:48:38 +0900

    画像をDBへ格納

--- htdocs/product/pro_add_done.php
+++ htdocs/product/pro_add_done.php
@@ -15,20 +15,22 @@ try
 
     $pro_name = $_POST['name'];
     $pro_price = $_POST['price'];
+    $pro_gazou_name = $_POST['gazou_name'];
 
     $pro_name = htmlspecialchars($pro_name,ENT_QUOTES,'UTF-8');
     $pro_price = htmlspecialchars($pro_price,ENT_QUOTES,'UTF-8');
-    // ↓教科書と開発環境が違う為要注意：[host=localhost]ではなく、dockerで使われているIPAddressを確認して[host=IPAddress]を入力する。
+
     $dsn = 'mysql:dbname=shop;host=172.19.0.4;port=3306;charset=utf8';
     $user = 'root';
     $password = 'password';
     $dbh = new PDO($dsn, $user, $password);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $sql = 'INSERT INTO mst_product(name,price) VALUES (?,?)';
+    $sql = 'INSERT INTO mst_product(name,price,gazou) VALUES (?,?,?)';
     $stmt = $dbh->prepare($sql);
     $data[] = $pro_name;
     $data[] = $pro_price;
+    $data[] = $pro_gazou_name;
     $stmt->execute($data);
 
     $dbh = null;
