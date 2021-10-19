72997fbbf3a2188b9efbddeffaa53ed77b073d98
ryu-sugi <81607989+ryu-sugi@users.noreply.github.com>
2021-10-19 19:14:34 +0900

    画像削除

--- htdocs/product/pro_add_check.php
+++ htdocs/product/pro_add_check.php
@@ -13,6 +13,7 @@
 
 $pro_name = $_POST['name'];
 $pro_price = $_POST['price'];
+$pro_gazou=$_FILES['gazou'];
 
 $pro_name = htmlspecialchars($pro_name,ENT_QUOTES, 'UTF-8');
 $pro_price = htmlspecialchars($pro_price,ENT_QUOTES, 'UTF-8');
@@ -37,6 +38,20 @@ else
     print '価格:';
     print $pro_price;
     print '円 <br> ';
+}
+
+if($pro_gazou['size']) > 0 )
+{
+     if($pro_gazou['size'] > 1000000)
+     {
+             print'画像が大きすぎます';
+     }
+     else
+     {
+            move_uploaded_file($pro_gazou['tmp_name'],'./gazou/'.$pro_gazou['name']);
+            print '<img src="./gazou/'.$pro_gazou['name'].'">';
+            print
+     }
 }
 
 if($pro_name==''||preg_match("/^[0-9]+$/", $pro_price)==0)
