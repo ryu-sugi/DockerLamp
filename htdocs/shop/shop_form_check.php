35f55263d682b1d9ed9b0630655a53076936051a
ryu-sugi <81607989+ryu-sugi@users.noreply.github.com>
2021-10-27 12:46:55 +0900

    受付確認画面にて文字列の表示

--- htdocs/shop/shop_form_check.php
+++ htdocs/shop/shop_form_check.php
@@ -25,15 +25,35 @@ if($onamae=='')
 {
     print 'お名前が入力されていません。 <br> <br>';
 }
+else
+{
+    print 'お名前 <br>';
+    print $onamae;
+    print '<br> <br>';
+}
 
 if(preg_match('/^([a-zA-Z0-9])+([a-zA-Z0-9._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9._-]+)+$/',$email)==0)
 {
     print 'メールアドレスを正確に入力してください。 <br> <br>';
 }
+else
+{
+    print 'メールアドレス <br>';
+    print $email;
+    print '<br> <br>';
+}
 
 if(preg_match('/^[0-9]+$/',$postal1)==0)
 {
     print '郵便番号は半角数字で入力してください。 <br> <br>';
+}
+else
+{
+    print '郵便番号 <br>';
+    print $postal1;
+    print '-';
+    print $postal2;
+    print '<br> <br>';
 }
 
 if(preg_match('/^[0-9]+$/',$postal2)==0)
@@ -45,10 +65,22 @@ if($address=='')
 {
     print '住所が入力されていません。 <br> <br>';
 }
+else
+{
+    print '住所 <br>';
+    print $address;
+    print '<br> <br>';
+}
 
 if(preg_match('/^[0-9]{2,4}-[0-9]{2,4}-[0-9]{3,4}$/',$tel)==0)
 {
     print '電話番号を正確に入力してください。 <br> <br>';
+}
+else
+{
+    print '電話番号 <br>';
+    print $tel;
+    print '<br> <br>';
 }
 
 print '<form method="post" action="shop_form_done.php">';
