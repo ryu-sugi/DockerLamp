1d69e9f8a43379fc0d4b8d65396190a2f34f51d1
ryu-sugi <81607989+ryu-sugi@users.noreply.github.com>
2021-10-27 13:23:03 +0900

    DBｻｰﾊﾞｰ障害対策 shop_form_done.php

--- htdocs/shop/shop_form_done.php
+++ htdocs/shop/shop_form_done.php
@@ -14,6 +14,9 @@
 
 <?php
 
+try
+{
+
 require_once('../common/common.php');
 
 $post=sanitize($_POST);
@@ -33,6 +36,11 @@ print  $postal1.'-'.$postal2. '<br>';
 print $address. '<br>';
 print $tel. '<br>';
 
+}
+catch(Exception $e)
+{
+    print 'ただいま障害により大変ご迷惑をお掛けしております。';
+    exit();
 ?>
 
 </body>
