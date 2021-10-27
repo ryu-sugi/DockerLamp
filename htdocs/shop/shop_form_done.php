27f12f8f6e548c61871b73c5239e99441546dff1
ryu-sugi <81607989+ryu-sugi@users.noreply.github.com>
2021-10-27 13:59:44 +0900

    送信メール作成

--- htdocs/shop/shop_form_done.php
+++ htdocs/shop/shop_form_done.php
@@ -36,6 +36,61 @@ print  $postal1.'-'.$postal2. '<br>';
 print $address. '<br>';
 print $tel. '<br>';
 
+$honbun='';
+$honbun.=$onamae."様 \n\n このたびはご注文ありがとうございました。 \n";
+$honbun.="\n";
+$honbun.="ご注文商品 \n";
+$honbun.="-------------\n";
+
+$cart= $_SESSION['cart'];
+$kazu= $_SESSION['kazu'];
+$max= count($cart);
+
+$dsn = 'mysql:dbname=shop;host=172.19.0.4;port=3306;charset=utf8';
+$user = 'root';
+$password = 'password';
+$dbh = new PDO($dsn, $user, $password);
+$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+for($i=0; $i<$max; $i++)
+{
+
+    $sql = 'SELECT name,price FROM mst_product WHERE code=?';
+    $stmt = $dbh->prepare($sql);
+    $data[0] = $cart[$i];
+    $stmt->execute($data);
+
+    $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    $name = $rec['name'];
+    $price = $rec['price'];
+    $suryo = $kazu[$i];
+    $shokei = $price * $suryo;
+
+    $honbun.=$name.' ';
+    $honbun.=$price.'円x';
+    $honbun.=$suryo.'個 =';
+    $honbun.=$shokei."円 \n";
+}
+
+$dbh = null;
+
+$honbun.="送料は無料です。 \n";
+$honbun.="------------- \n";
+$honbun.="\n";
+$honbun.="代金は以下の口座にお振り込みください。 \n";
+$honbun.="ろくまる銀行 やさい支店 普通口座 1234567 \n";
+$honbun.="入金確認が取れ次第、梱包、発送させていただきます。 \n";
+$honbun.="\n";
+$honbun.="□□□□□□□□□□□□□□□□□□□□□□□□□□□□ \n";
+$honbun.=" ~安心野菜のろくまる農園~ \n";
+$honbun.="\n";
+$honbun.="〇〇県六丸郡六丸村 123-4 \n";
+$honbun.="電話 090-6060-xxxx \n"; 
+$honbun.="メール info@rokumarunouen.co.jp \n";
+$honbun.="□□□□□□□□□□□□□□□□□□□□□□□□□□□□ \n";
+print '<br>';
+print nl2br($honbun);
 }
 catch(Exception $e)
 {
